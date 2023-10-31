31c978e18c623ec4313f0438a98eb9146dc153d5
derllock <denissmaheshwari@gmail.com>
2023-10-31 17:22:20 +0530

    good to go

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 // Define a MongoDB model
-const Skill = mongoose.model('Skill', {
+const SkillSchema = new mongoose.Schema({
     technical_skill: String,
     location: String,
-    experience: String,
+    experience: Number,
 });
+const Skill = mongoose.model('Skill',SkillSchema);
 
 // Serve an HTML form with a dropdown for skill selection
 app.get('/', (req, res) => {
@@ -25,11 +26,15 @@ app.get('/', (req, res) => {
 
 // Handle form submission and fetch data from MongoDB
 app.post('/search', async (req, res) => {
-    const selectedSkill = req.body.skill;
-
+    const selected = [req.body.skill, req.body.location, req.body.experience];
     try {
-        const results = await Skill.find({ technical_skill: selectedSkill });
-        res.render('results', { results });
+        const results = await Skill.find({
+            technical_skill: selected[0],
+            location: selected[1],
+            experience: selected[2],
+        });
+        if(!results)res.send('No results found');
+        else res.render('results',{ results });
     } catch (err) {
         console.error(err);
     }
